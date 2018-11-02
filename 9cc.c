ed80973f03a5a866deeaaac5041a6a2cce4b9970
NAKAJIMA Yusaku <yupong7@gmail.com>
2018-11-02 20:18:47 +0900

    add tokenize()

--- 9cc.c
+++ 9cc.c
@@ -1,5 +1,53 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+enum {
+  TK_NUM = 256,
+  TK_EOF,
+};
+
+typedef struct {
+  int ty; // type of token
+  int val; // ty が TK_NUM の場合のその値
+  char *input; // token string(for error message)
+} Token;
+
+Token tokens[100];
+
+void tokenize(char *p) {
+  int i = 0;
+  while (*p) {
+    if (isspace(*p)) {
+      p++;
+      continue;
+    }
+    if (*p == '+' || *p == '-') {
+      tokens[i].ty = *p;
+      tokens[i].input = p;
+      i++;
+      p++;
+      continue;
+    }
+    if (isdigit(*p)) {
+      tokens[i].ty = TK_NUM;
+      tokens[i].input = p;
+      tokens[i].val = strtol(p, &p, 10);
+      i++;
+      continue;
+    }
+    fprintf(stderr, "トークナイズできません: %s\n", p);
+    exit(1);
+  }
+  tokens[i].ty = TK_EOF;
+  tokens[i].input = p;
+}
+
+void error(int i) {
+  fprintf(stderr, "unexpected token: %s\n", tokens[i].input);
+  exit(1);
+}
 
 int main(int argc, char ** argv) {
   if (argc != 2) {
@@ -7,25 +55,38 @@ int main(int argc, char ** argv) {
     return 1;
   }
 
-  char *p = argv[1];
+  tokenize(argv[1]);
   
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
   printf("main:\n");
-  printf("\tmov rax, %ld\n", strtol(p, &p, 10));
 
-  while (*p) {
-    if (*p == '+') {
-      p++;
-      printf("\tadd rax, %ld\n", strtol(p, &p, 10));
+  if (tokens[0].ty != TK_NUM)
+    error(0);
+  
+  printf("\tmov rax, %d\n", tokens[0].val);
+
+  int i = 1;
+  while (tokens[i].ty != TK_EOF) {
+    if (tokens[i].ty == '+') {
+      i++;
+      if (tokens[i].ty != TK_NUM)
+	error(i);
+      printf("\tadd rax, %d\n", tokens[i].val);
+      i++;
       continue;
     }
-    if (*p == '-') {
-      p++;
-      printf("\tsub rax, %ld\n", strtol(p, &p, 10));
+    
+    if (tokens[i].ty == '-') {
+      i++;
+      if (tokens[i].ty != TK_NUM)
+	error(i);
+      printf("\tsub rax, %d\n", tokens[i].val);
+      i++;
       continue;
     }
-    fprintf(stderr, "unexpected character: '%c'\n", *p);
+
+    error(i);
     return 1;
   }
   
