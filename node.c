24e285b1d72a4ee2a0258fa9023f1fce99efbac7
NAKAJIMA Yusaku <yupong7@gmail.com>
2018-11-05 16:57:58 +0900

    split node.c from parse.c

---
+++ node.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "9cc.h"
+
+Node *new_node(int ty, Node *lhs, Node *rhs) {
+  Node *node = malloc(sizeof(Node));
+  node->ty = ty;
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return node;
+}
+
+Node *new_node_num(int val) {
+  Node *node = malloc(sizeof(Node));
+  node->ty = ND_NUM;
+  node->val = val;
+  return node;
+}
+
+Node_Ite *setup(Node *start) {
+  return NULL;
+}
+
+Node *next_node(Node_Ite *ite) {
+  return NULL;
+}
