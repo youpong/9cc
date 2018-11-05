721b02c5d3a861d92429947dcba10b992f5368d2
NAKAJIMA Yusaku <yupong7@gmail.com>
2018-11-05 18:06:44 +0900

    correct return value when end.

--- node.c
+++ node.c
@@ -25,8 +25,16 @@ Node_Ite *new_ite(Node *start) {
 
 // returns NULL, if end.
 Node *next_node(Node_Ite *ite) {
+  Node *ret;
+
+  if (ite->cur == NULL ) {
+    return NULL;
+  }
+  
   if (ite->cur->ty == ND_NUM) {
-    return ite->cur;
+    ret = ite->cur;
+    ite->cur = NULL;
+    return ret;
   }
   // ite->cur->ty != ND_NUM
   return NULL; // TODO
@@ -34,17 +42,33 @@ Node *next_node(Node_Ite *ite) {
 
 #ifdef UNIT_TEST
 
+void print_node(Node *node) {
+  Node *i;
+  Node_Ite *ite = new_ite(node);
+  while	((i = next_node(ite)) != NULL) {
+    if(i->ty == ND_NUM) {
+      printf("%d\n", i->val);
+      continue;
+    }
+    printf("%c\n", i->ty);
+  }
+}
+
 void test10() {
   Node *node = new_node_num(27);
-  Node_Ite *ite = new_ite(node);
-  
-  Node *i = next_node(ite);
-  printf("%d\n", i->val);
+
+  print_node(node);
 }
+
+void test20() {
+  Node *node = new_node('+', new_node_num(30), new_node_num(31));
+  Node_Ite *ite = new_ite(node);
+}  
 
 int main() {
   test10();
+  //  test20();
+  
   return 0;
 }
-
 #endif
