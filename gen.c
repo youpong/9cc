673109d2fc4c806dde92ef11c789f00c6b6aa39f
NAKAJIMA Yusaku <yupong7@gmail.com>
2018-11-06 13:50:00 +0900

    implement gen()

--- gen.c
+++ gen.c
@@ -1,39 +1,86 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "9cc.h"
 
 void gen(Node *node) {
   if (node->ty == ND_NUM) {
-    printf("\tmov rax, %d\n", node->val);
+    printf("\tpush %d\n", node->val);
     return;
   }
 
-  if (node->ty == '+') {
-    printf("\tmov rax, %d\n", node->lhs->val);
-    printf("\tadd rax, %d\n", node->rhs->val);
-    return;
+  gen(node->lhs);
+  gen(node->rhs);
+
+  printf("\tpop rdi\n");
+  printf("\tpop rax\n");
+
+  switch(node->ty) {
+  case '+':
+    printf("\tadd rax, rdi\n");
+    break;
+  case '-':
+    printf("\tsub rax, rdi\n");
+    break;
+  case '*':
+    printf("\tmul rax, rdi\n");
+    break;
+  case '/':
+    printf("\tmov rdx, 0\n");
+    printf("\tdiv rdi\n");
+    break;
+  defalt:
+    fprintf(stderr, "unexpected type of node");
+    exit(1);
   }
 
-  if (node->ty == '-') {
-    printf("\tmov rax, %d\n", node->lhs->val);
-    printf("\tsub rax, %d\n", node->rhs->val);
-    return;
-  }
-
-
-  return;
+  printf("\tpush rax\n");
 }
 
 #ifdef UNIT_TEST
 int pos;
 Token tokens[100];
-	     
-int main() {
-  Node *node;
-  node = new_node_num(1047);
-  gen(node);
 
-  node = new_node('+', new_node_num(1), new_node_num(2));
-  gen(node);
+void test10() {
+  gen(new_node_num(1047));
+}
+
+void test20() {
+  gen(new_node('+', new_node_num(1), new_node_num(2)));
+}
+    
+
+void test30() {
+  gen( new_node('/', new_node_num(3), new_node_num(4)));
+}
+
+void test40() {
+  Node *node = new_node('+', new_node_num(5), new_node_num(6));
+  gen(new_node('-', node, new_node_num(7)));
+}
+
+void test41() {
+  Node *node = new_node('+', new_node_num(8), new_node_num(9));
+  gen(new_node('-', new_node_num(10), node));
+}
+
+void test50() {
+  gen(new_node('*', new_node_num(11), new_node_num(12)));
+}
+
+void test51() {
+  gen(new_node('/', new_node_num(13), new_node_num(14)));
+}
+
+void test100() {
+  Node *me = new_node('+', new_node_num(15), new_node_num(16));
+  Node *fater = new_node('-', me, new_node_num(18));
+  Node *grand = new_node('*', fater, new_node_num(19));
+  gen(grand);
+}
+
+int main() {
+  test100();
+  
   return 0;
 }
 #endif
