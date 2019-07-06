beadd6e471e600fc9318a5f2915e80c9137c3e75
NAKAJIMA Yusaku <yupong7@gmail.com>
2019-07-06 17:18:02 +0900

    test print result of tests

---
+++ test/print_result.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int print_all_tests_passed() {
+  printf("============================\n");
+  printf(" All function tests passed. \n");
+  printf("============================\n");
+
+  return 0;
+}
